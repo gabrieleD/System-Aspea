46c302d2428205dffb96dabd5ba95028910d3990
user@name.org <andrea.scanavacca@gmail.com>
2015-08-10 17:47:59 +0200

    scrittura beans parte 2

--- Sysem_Aspea/src/com/system/aspea/beans/Allegato6.java
+++ Sysem_Aspea/src/com/system/aspea/beans/Allegato6.java
@@ -1,5 +1,150 @@
 package com.system.aspea.beans;
 
-public class Allegato6 {
+public class Allegato6 extends SchedaAstratta {
 
+	private boolean sistemaDiAllarme;
+	private boolean sistemaDiBlocco;
+	private boolean sistemaSequenziale;
+	private boolean sistemaSegnLuminosi;
+	private boolean aRele;
+	private boolean logicaCablata;
+	private boolean plc;
+	private String specificaNum;
+	private String disegnoNum;
+	private String ubicazione;
+	private String verificaFunzionale;
+	private boolean compilatoRapportoInefficienza;
+	private String[] strumentiUsati;
+	private String[] matricoleStrUsati;
+	private String verificheVarie;
+	private String note;
+
+	public boolean isSistemaDiAllarme() {
+		return sistemaDiAllarme;
+	}
+
+	public void setSistemaDiAllarme(boolean sistemaDiAllarme) {
+		this.sistemaDiAllarme = sistemaDiAllarme;
+	}
+
+	public boolean isSistemaDiBlocco() {
+		return sistemaDiBlocco;
+	}
+
+	public void setSistemaDiBlocco(boolean sistemaDiBlocco) {
+		this.sistemaDiBlocco = sistemaDiBlocco;
+	}
+
+	public boolean isSistemaSequenziale() {
+		return sistemaSequenziale;
+	}
+
+	public void setSistemaSequenziale(boolean sistemaSequenziale) {
+		this.sistemaSequenziale = sistemaSequenziale;
+	}
+
+	public boolean isSistemaSegnLuminosi() {
+		return sistemaSegnLuminosi;
+	}
+
+	public void setSistemaSegnLuminosi(boolean sistemaSegnLuminosi) {
+		this.sistemaSegnLuminosi = sistemaSegnLuminosi;
+	}
+
+	public boolean isaRele() {
+		return aRele;
+	}
+
+	public void setaRele(boolean aRele) {
+		this.aRele = aRele;
+	}
+
+	public boolean isLogicaCablata() {
+		return logicaCablata;
+	}
+
+	public void setLogicaCablata(boolean logicaCablata) {
+		this.logicaCablata = logicaCablata;
+	}
+
+	public boolean isPlc() {
+		return plc;
+	}
+
+	public void setPlc(boolean plc) {
+		this.plc = plc;
+	}
+
+	public String getSpecificaNum() {
+		return specificaNum;
+	}
+
+	public void setSpecificaNum(String specificaNum) {
+		this.specificaNum = specificaNum;
+	}
+
+	public String getDisegnoNum() {
+		return disegnoNum;
+	}
+
+	public void setDisegnoNum(String disegnoNum) {
+		this.disegnoNum = disegnoNum;
+	}
+
+	public String getUbicazione() {
+		return ubicazione;
+	}
+
+	public void setUbicazione(String ubicazione) {
+		this.ubicazione = ubicazione;
+	}
+
+	public String getVerificaFunzionale() {
+		return verificaFunzionale;
+	}
+
+	public void setVerificaFunzionale(String verificaFunzionale) {
+		this.verificaFunzionale = verificaFunzionale;
+	}
+
+	public boolean isCompilatoRapportoInefficienza() {
+		return compilatoRapportoInefficienza;
+	}
+
+	public void setCompilatoRapportoInefficienza(
+			boolean compilatoRapportoInefficienza) {
+		this.compilatoRapportoInefficienza = compilatoRapportoInefficienza;
+	}
+
+	public String[] getStrumentiUsati() {
+		return strumentiUsati;
+	}
+
+	public void setStrumentiUsati(String[] strumentiUsati) {
+		this.strumentiUsati = strumentiUsati;
+	}
+
+	public String[] getMatricoleStrUsati() {
+		return matricoleStrUsati;
+	}
+
+	public void setMatricoleStrUsati(String[] matricoleStrUsati) {
+		this.matricoleStrUsati = matricoleStrUsati;
+	}
+
+	public String getVerificheVarie() {
+		return verificheVarie;
+	}
+
+	public void setVerificheVarie(String verificheVarie) {
+		this.verificheVarie = verificheVarie;
+	}
+
+	public String getNote() {
+		return note;
+	}
+
+	public void setNote(String note) {
+		this.note = note;
+	}
 }
--- Sysem_Aspea/src/com/system/aspea/beans/Allegato7.java
+++ Sysem_Aspea/src/com/system/aspea/beans/Allegato7.java
@@ -2,4 +2,241 @@ package com.system.aspea.beans;
 
 public class Allegato7 {
 
+	private String sigla;
+	private String taratura;
+	private String classe;
+	private String limitiDiCampo;
+	private String matricola;
+	private String costruttore;
+	private String tipo;
+	private String ubicazione;
+	private String specifica;
+	private boolean rotturaCorpo;
+	private boolean targIdentNonConformeSepc;
+	private boolean lineaAlimGuasta;
+	private boolean colegamentiProcessiInadeguati;
+	private boolean montaggioErrato;
+	private boolean rotturaMembrana;
+	private boolean deformazioneMembrana;
+	private boolean nonLinearita;
+	private boolean parteElettronicaGuasta;
+	private boolean nonRipetibilita;
+	private boolean rilevazioneErroreOltreClasse;
+	private boolean nonAttendibilitaPressStatica;
+	private boolean posizionatoreRotto;
+	private boolean posizionatoreGuasto;
+	private String[] strumentiUsati;
+	private String[] matricoleStrUsati;
+	private String[] UDM;
+
+	public String getSigla() {
+		return sigla;
+	}
+
+	public void setSigla(String sigla) {
+		this.sigla = sigla;
+	}
+
+	public String getTaratura() {
+		return taratura;
+	}
+
+	public void setTaratura(String taratura) {
+		this.taratura = taratura;
+	}
+
+	public String getClasse() {
+		return classe;
+	}
+
+	public void setClasse(String classe) {
+		this.classe = classe;
+	}
+
+	public String getLimitiDiCampo() {
+		return limitiDiCampo;
+	}
+
+	public void setLimitiDiCampo(String limitiDiCampo) {
+		this.limitiDiCampo = limitiDiCampo;
+	}
+
+	public String getMatricola() {
+		return matricola;
+	}
+
+	public void setMatricola(String matricola) {
+		this.matricola = matricola;
+	}
+
+	public String getCostruttore() {
+		return costruttore;
+	}
+
+	public void setCostruttore(String costruttore) {
+		this.costruttore = costruttore;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+	public String getUbicazione() {
+		return ubicazione;
+	}
+
+	public void setUbicazione(String ubicazione) {
+		this.ubicazione = ubicazione;
+	}
+
+	public String getSpecifica() {
+		return specifica;
+	}
+
+	public void setSpecifica(String specifica) {
+		this.specifica = specifica;
+	}
+
+	public boolean isRotturaCorpo() {
+		return rotturaCorpo;
+	}
+
+	public void setRotturaCorpo(boolean rotturaCorpo) {
+		this.rotturaCorpo = rotturaCorpo;
+	}
+
+	public boolean isTargIdentNonConformeSepc() {
+		return targIdentNonConformeSepc;
+	}
+
+	public void setTargIdentNonConformeSepc(boolean targIdentNonConformeSepc) {
+		this.targIdentNonConformeSepc = targIdentNonConformeSepc;
+	}
+
+	public boolean isLineaAlimGuasta() {
+		return lineaAlimGuasta;
+	}
+
+	public void setLineaAlimGuasta(boolean lineaAlimGuasta) {
+		this.lineaAlimGuasta = lineaAlimGuasta;
+	}
+
+	public boolean isColegamentiProcessiInadeguati() {
+		return colegamentiProcessiInadeguati;
+	}
+
+	public void setColegamentiProcessiInadeguati(
+			boolean colegamentiProcessiInadeguati) {
+		this.colegamentiProcessiInadeguati = colegamentiProcessiInadeguati;
+	}
+
+	public boolean isMontaggioErrato() {
+		return montaggioErrato;
+	}
+
+	public void setMontaggioErrato(boolean montaggioErrato) {
+		this.montaggioErrato = montaggioErrato;
+	}
+
+	public boolean isRotturaMembrana() {
+		return rotturaMembrana;
+	}
+
+	public void setRotturaMembrana(boolean rotturaMembrana) {
+		this.rotturaMembrana = rotturaMembrana;
+	}
+
+	public boolean isDeformazioneMembrana() {
+		return deformazioneMembrana;
+	}
+
+	public void setDeformazioneMembrana(boolean deformazioneMembrana) {
+		this.deformazioneMembrana = deformazioneMembrana;
+	}
+
+	public boolean isNonLinearita() {
+		return nonLinearita;
+	}
+
+	public void setNonLinearita(boolean nonLinearita) {
+		this.nonLinearita = nonLinearita;
+	}
+
+	public boolean isParteElettronicaGuasta() {
+		return parteElettronicaGuasta;
+	}
+
+	public void setParteElettronicaGuasta(boolean parteElettronicaGuasta) {
+		this.parteElettronicaGuasta = parteElettronicaGuasta;
+	}
+
+	public boolean isNonRipetibilita() {
+		return nonRipetibilita;
+	}
+
+	public void setNonRipetibilita(boolean nonRipetibilita) {
+		this.nonRipetibilita = nonRipetibilita;
+	}
+
+	public boolean isRilevazioneErroreOltreClasse() {
+		return rilevazioneErroreOltreClasse;
+	}
+
+	public void setRilevazioneErroreOltreClasse(
+			boolean rilevazioneErroreOltreClasse) {
+		this.rilevazioneErroreOltreClasse = rilevazioneErroreOltreClasse;
+	}
+
+	public boolean isNonAttendibilitaPressStatica() {
+		return nonAttendibilitaPressStatica;
+	}
+
+	public void setNonAttendibilitaPressStatica(
+			boolean nonAttendibilitaPressStatica) {
+		this.nonAttendibilitaPressStatica = nonAttendibilitaPressStatica;
+	}
+
+	public boolean isPosizionatoreRotto() {
+		return posizionatoreRotto;
+	}
+
+	public void setPosizionatoreRotto(boolean posizionatoreRotto) {
+		this.posizionatoreRotto = posizionatoreRotto;
+	}
+
+	public boolean isPosizionatoreGuasto() {
+		return posizionatoreGuasto;
+	}
+
+	public void setPosizionatoreGuasto(boolean posizionatoreGuasto) {
+		this.posizionatoreGuasto = posizionatoreGuasto;
+	}
+
+	public String[] getStrumentiUsati() {
+		return strumentiUsati;
+	}
+
+	public void setStrumentiUsati(String[] strumentiUsati) {
+		this.strumentiUsati = strumentiUsati;
+	}
+
+	public String[] getMatricoleStrUsati() {
+		return matricoleStrUsati;
+	}
+
+	public void setMatricoleStrUsati(String[] matricoleStrUsati) {
+		this.matricoleStrUsati = matricoleStrUsati;
+	}
+
+	public String[] getUDM() {
+		return UDM;
+	}
+
+	public void setUDM(String[] uDM) {
+		UDM = uDM;
+	}
 }
